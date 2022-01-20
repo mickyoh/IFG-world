02935bef22f38fea4baeee9464e276fff1333af6
mitsuki <96933082+mickyoh@users.noreply.github.com>
2022-01-20 20:07:06 +0900

    Add files via upload

---
+++ html.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IFG world</title>
+</head>
+<body>
+    <header>
+        <p>IFG</p>
+        <br>
+        <p>We are Neandermann.</p>
+        
+
+    </header>
+    
+    <main>
+
+    </main>
+
+    <footer>
+
+    </footer>
+
+</body>
+</html>
